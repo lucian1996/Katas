c0f9da7a61c00aa23b458b5af256dfedc787a4a3
Lucian <Lucian.io@outlook.com>
2023-08-11 16:35:06 -0400

    hashtag

---
+++ Hashtag/hashtag.js
@@ -0,0 +1,9 @@
+function generateHashtag(str) {
+  if (str.length > 1 || str.length < 140) {
+    words = str.split(" ")
+    console.log(words)
+    
+  }
+  return false
+}
+generateHashtag("Hello world")
