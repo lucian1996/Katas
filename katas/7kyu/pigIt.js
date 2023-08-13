c74e420a8191be5d6d0241bca5bb337b9ca46f86
Lucian <Lucian.io@outlook.com>
2023-08-12 20:48:00 -0400

    🚀 6kyu Level Up

---
+++ katas/7kyu/pigIt.js
@@ -0,0 +1,22 @@
+function pigIt(str) {
+  words = str.match(/\b[A-Za-z]+\b/g)
+  const transformedWords = words.map((word) => {
+    const prefix = word[0]
+    const root = word.slice(1)
+    return root + prefix + "ay"
+  })
+  const final = transformedWords.join(" ")
+
+  const lastChar = str[str.length - 1]
+  const isSpecialCharacter = /[!.,;?]/.test(lastChar)
+
+  console.log(final + (isSpecialCharacter ? " " + lastChar : ""))
+  return final + (isSpecialCharacter ? " " + lastChar : "")
+}
+
+// pigIt("Pig latin is cool")
+// ;("igPay atinlay siay oolcay")
+// pigIt("This is my string")
+// ;("hisTay siay ymay tringsay")
+pigIt("O tempora o mores !")
+// ;("Oay emporatay oay oresmay !")
